5b2e8a2ef8bdb8c22fee13ebb116c5dcfae38fc2
YongquanHuangZJU <yongquan.huang@zju.edu.cn>
2025-10-31 17:35:01 +0800

    Update __init__.py

--- pySuStaIn/__init__.py
+++ pySuStaIn/__init__.py
@@ -1,3 +1,6 @@
+# MP-SuStaIn modified: add ZscoreSustainMP
+# The following is the original comments
+
 # Authors: Leon Aksman <l.aksman@ucl.ac.uk>
 # License: TBC
 
@@ -6,3 +9,4 @@ from .MixtureSustain import *
 from .ZscoreSustain import *
 from .OrdinalSustain import *
 from .ZScoreSustainMissingData import *
+from .ZscoreSustainMP import *
